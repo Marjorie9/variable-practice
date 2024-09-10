3690d3e2dee22f4fcccfe35307d79cd231eb4359
Marjorie9 <marjoriekatedeguzman@gmail.com>
2024-09-10 19:02:24 +0800

    Add files via upload

---
+++ app.js
@@ -0,0 +1,17 @@
+// Create a variable named name and store your name
+let name = 'De guzman, Marjorie Kate C.';
+
+// Create a variable named whatDoYouWannaBecomeInYourLife and store "millioner"
+let whatDoYouWannaBecomeInYourLife = 'millioner';
+
+// Create a variable named gender and store your gender
+let gender = 'Female';
+
+// Create a variable named FaceBookHandle and store your FaceBook Handle
+let FaceBookHandle = 'Marjorie Kate De guzman';
+
+// log all the viriables to the console
+console.log('What is your Name? ', name);
+console.log('What do you wanna become in your life? ', whatDoYouWannaBecomeInYourLife);
+console.log('Are you a Male or Female? ', gender);
+console.log('My FaceBook: ', FaceBookHandle);
